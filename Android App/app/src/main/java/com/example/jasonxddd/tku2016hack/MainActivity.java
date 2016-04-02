1b40acbbed66637087485186a696a627ab5fbd0b
Len_AshBell <jason841022@yahoo.com.tw>
2016-04-02 14:33:55 +0800

    androidApp

---
+++ Android App/app/src/main/java/com/example/jasonxddd/tku2016hack/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.jasonxddd.tku2016hack;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
+
+}
