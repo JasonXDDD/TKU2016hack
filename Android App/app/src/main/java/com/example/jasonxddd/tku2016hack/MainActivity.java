a2eec72ad2440606e05724b8c081f864daa56ab5
Hati0608 <pohan4630510@gmail.com>
2016-04-02 19:16:46 +0800

    no message

--- Android App/app/src/main/java/com/example/jasonxddd/tku2016hack/MainActivity.java
+++ Android App/app/src/main/java/com/example/jasonxddd/tku2016hack/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.jasonxddd.tku2016hack;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+<<<<<<< Updated upstream
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
+=======
+>>>>>>> Stashed changes
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -35,6 +37,9 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, mainPage.class);
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+
                 startActivity(intent);
                 finish();
             }
